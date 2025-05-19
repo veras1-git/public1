fd8388db586ba337ada5a5b954331a631f49d82c
veravol <vera.v1985@gmail.com>
2025-05-19 17:47:00 +0300

    sss4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     # fff
     #fff
+    #add1
+    #add2
 
 
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
